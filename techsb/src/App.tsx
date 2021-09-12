6672571e9a498764a40c733ea39d6c1600bb4208
Santosh Bujala <bujalasanthoshreddy@gmail.com>
2021-09-12 22:28:26 +0530

    [Header and Footer Changes]

--- techsb/src/App.tsx
+++ techsb/src/App.tsx
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
+        <header className="app-header">
+          <div className="header-text-align">
+            Comming Soon
+          </div>
+        </header>
+        <div className="App-body">
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.linkedin.com/in/santosh-reddy-7766574a/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Bujala Santosh Reddy Github
           </a>
-        </header>
+          <a
+            className="App-link"
+            href="https://github.com/SantoshReddyBujala/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Bujala Santosh Reddy LinkedIn </a>
+        </div>
+        <footer className="app-footer">Comming Soon</footer>
       </div>
     );
   }
