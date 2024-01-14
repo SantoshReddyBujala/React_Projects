cca7e12b51520b0bbdc1d77681e78d3437151344
Bujala Santosh Reddy <bujalasanthoshreddy@gmail.com>
2024-01-14 16:42:46 -0500

    React Web App completed

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -17,6 +17,11 @@ import { MoutableRef } from "./components/ref/MoutableRef";
 import { CounterClass } from "./components/class/CounterClass";
 import { Private } from "./components/auth/Private";
 import { Profile } from "./components/auth/Profile";
+import { List } from "./components/generics/List";
+import { RamdomNumber } from "./components/restriction/RamdomNumber";
+import { Toast } from "./components/templateliterals/Toast";
+import { CustomButton } from "./components/html/Button";
+import { Text } from "./components/pholymorpic/Text";
 
 
 function App() {
@@ -43,13 +48,17 @@ function App() {
       <Greet name="Santosh Reddy Bujala" version={18} isLogedin={true} />
       <Person name={personName} />
       <PersonsList names={nameList} />
-      <Status status="success"/>
+      <Status status="success" />
       <Heading>Heading Text</Heading>
       <Oscar>
         <Heading>Inner Components</Heading>
       </Oscar>
-      <Button handleClick={(event,id)=>{console.log('Button clicked', event, id)}}/>
-      <Input value="test" handleChange={(event)=>console.log(event)}/>
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button clicked", event, id);
+        }}
+      />
+      <Input value="test" handleChange={(event) => console.log(event)} />
       <LogedIn />
       <User />
       <Counter />
@@ -61,7 +70,41 @@ function App() {
       </UserContextProvider>
       <MoutableRef />
       <CounterClass message="The Count Value is" />
-      <Private isLogedin={true} component={Profile}/>
+      <Private isLogedin={true} component={Profile} />
+      {/* <List
+        items={["Santosh", "Swetha", "Manvith"]}
+        onClick={(item) => console.log(item)}
+      />
+      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
+      <List
+        items={[
+          {
+            id: 1,
+            first: "Santosh",
+            last: "Bujala",
+          },
+          {
+            id: 2,
+            first: "Swetha",
+            last: "Gangireddy",
+          },
+          {
+            id: 3,
+            first: "Manvith",
+            last: "Bujala",
+          },
+        ]}
+        onClick={(item) => console.log(item)}
+      ></List>
+      <h1>Restricting Props</h1>
+      <RamdomNumber value={10} isPositive></RamdomNumber>
+      <RamdomNumber value={-10} isNegative></RamdomNumber>
+      <RamdomNumber value={0} isZero></RamdomNumber>
+      <Toast position="center"></Toast>
+      <CustomButton variant='primary' onClick={()=>console.log('Clicked')}>Primary Button</CustomButton>
+      <Text as='h1' size='lg'>Heading</Text>
+      <Text as='p' size='md'>Paragraph</Text>
+      <Text as='label' htmlFor="someId" size='sm' color="secondary">Label</Text>
     </div>
   );
 }
